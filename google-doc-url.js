360889b2c89e31a8457a6a4a116d880e15a8538f
Mauricio A <mauricio.arriaga@icloud.com>
2017-03-23 10:29:51 -0600

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1y1FlAkyNtkjfglNf2ANv7xILN6sa15IzE5hJ_Bl3VWY/pubhtml';
